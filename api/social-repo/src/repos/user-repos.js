d2273294ed59d047e996e6432c391ac089d1bf46
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-10-11 22:46:39 +0200

    accessing the api

--- api/social-repo/src/repos/user-repos.js
+++ api/social-repo/src/repos/user-repos.js
@@ -1,10 +1,12 @@
 const pool = require('../pool');
+const toCamelCase = require('./utils/to-camel-case')
 
 class UserRepo {
   static async find() {
     const { rows } = await pool.query('SELECT * FROM users;');
 
-    return rows;
+    
+    return toCamelCase(rows);
   }
 
   static async findById() {}
