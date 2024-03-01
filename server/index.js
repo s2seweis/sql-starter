62f4772031fb2fe5fdc80414282cd6c1a76813f0
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-03-01 11:30:01 +0100

    add postgre db connection for heroku

--- server/index.js
+++ server/index.js
@@ -3,12 +3,23 @@ const pool = require('./src/pool/pool.js');
 
 // Database configuration for 'DeliveryShopDB'
 const deliveryShopConfig = {
-  host: 'localhost',
+  host: 'ec2-107-21-67-46.compute-1.amazonaws.com',
   port: 5432,
-  database: 'DeliveryShopDB',
-  user: 'SWT',
-  password: '24081987',
+  database: 'd1gch85sbm0pt5',
+  user: 'uvftaacwpaegoj',
+  password: '346f1cff02ecdc129738d2d8a0596a6ee854252f8434e643890b97010c96f536',
+  // ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
+// const deliveryShopConfig = {
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'DeliveryShopDB',
+//   user: 'SWT',
+//   password: '24081987',
+// };
 
 pool
   .connect(deliveryShopConfig)
