2dfd24848f6a275b49a668e8f2c894c969a82f54
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-04-10 09:45:43 +0200

    update credentials

--- server/index.js
+++ server/index.js
@@ -4,15 +4,25 @@ const path = require("path");
 
 // Database configuration for 'DeliveryShopDB'
 const deliveryShopConfig = {
-  host: 'ec2-107-21-67-46.compute-1.amazonaws.com',
+  host: 'ec2-3-221-177-27.compute-1.amazonaws.com',
   port: 5432,
-  database: 'd1gch85sbm0pt5',
-  user: 'uvftaacwpaegoj',
-  password: '346f1cff02ecdc129738d2d8a0596a6ee854252f8434e643890b97010c96f536',
+  database: 'ddk22o5m1aqq4q',
+  user: 'pfirqjhubpfqvp',
+  password: '8ab07610e53a224e98c52b12f6bc973d646b92eedf5bb707b3288fc370930a2a',
   ssl: {
     rejectUnauthorized: false,
   },
 };
+// const deliveryShopConfig = {
+//   host: 'ec2-107-21-67-46.compute-1.amazonaws.com',
+//   port: 5432,
+//   database: 'd1gch85sbm0pt5',
+//   user: 'uvftaacwpaegoj',
+//   password: '346f1cff02ecdc129738d2d8a0596a6ee854252f8434e643890b97010c96f536',
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// };
 
 pool
   .connect(deliveryShopConfig)
